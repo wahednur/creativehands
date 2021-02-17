02c515a1d395919b973e6b6a1a516746e0ee7fb8
Wahed Nur <wahednur@gmail.com>
2021-02-18 00:42:51 +0600

    Project recreated

---
+++ assets/js/main.js
@@ -0,0 +1,23 @@
+$('.owl-carousel').owlCarousel({
+    
+    margin:10,
+    nav:true,
+    autoplay:true,
+    loop:true,
+    nav:false,
+    navText: ['<i class="fas fa-angle-left"></i>', '<i class="fas fa-angle-right"></i>'],
+    responsive:{
+        0:{
+            items:2
+        },
+        768:{
+            items:4
+        },
+        992:{
+            items:4
+        },
+        1000:{
+            items:5
+        }
+    }
+})
